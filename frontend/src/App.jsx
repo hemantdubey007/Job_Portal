1e23088e52271c9ae436691b4130e8e9ced89576
Hemant Dubey <dubeyhemantjpr@gmail.com>
2024-09-16 12:41:48 +0530

    Created Some Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import Companies from './components/admin/Companies'
 import CompnayCreate from './components/admin/CompanyCreate'
 import CompanySetup from './components/admin/CompanySetup'
 import AdminJobs from './components/admin/AdminJobs'
+import PostJob from './components/admin/PostJob'
+import Applicants from './components/admin/Applicant'
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -65,6 +67,16 @@ function App() {
     {
       path:'/admin/jobs',
       element:<AdminJobs/>
+    },
+
+    {
+      path:'/admin/jobs/create',
+      element:<PostJob/>
+    },
+
+    {
+      path:'/admin/jobs/:id/applicants',
+      element:<Applicants/>
     }
   ])
   
--- frontend/src/components/admin/AdminJobs.jsx
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -1,33 +1,38 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../shared/Navbar";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import CompaniesTable from "./CompaniesTable";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { setsearchCompanyByText } from "@/redux/companySlice";
-import AdminJobsTable from "./AdminJobsTable";
+import React, { useEffect, useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button' 
+import { useNavigate } from 'react-router-dom' 
+import { useDispatch } from 'react-redux' 
+import AdminJobsTable from './AdminJobsTable'
+import useGetAllAdminJobs from '@/hooks/useGetAllAdminJobs'
+import { setSearchJobByText } from '@/redux/jobSlice'
 
 const AdminJobs = () => {
-    const navigate = useNavigate();
-    const [input, setInput] = useState("");
-    const dispatch = useDispatch();
+  useGetAllAdminJobs();
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(setsearchCompanyByText(input))
-    }, [input])
+  useEffect(() => {
+    dispatch(setSearchJobByText(input));
+  }, [input]);
   return (
     <div>
       <Navbar />
-      <div className="max-w-6xl mx-auto my-10">
-        <div className="flex items-center justify-between my-5">
-          <Input className="w-fit" placeholder="filter by name" onChange = {() => setInput(e.target.value)} />
-          <Button onClick = {() => navigate("/admin/companies/create")}>New Jobs</Button>
+      <div className='max-w-6xl mx-auto my-10'>
+        <div className='flex items-center justify-between my-5'>
+          <Input
+            className="w-fit"
+            placeholder="Filter by name, role"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button onClick={() => navigate("/admin/jobs/create")}>New Jobs</Button>
         </div>
-        <AdminJobsTable/>
+        <AdminJobsTable />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AdminJobs;
+export default AdminJobs
